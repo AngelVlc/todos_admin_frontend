58161cf98d49b939b861a32ee405920cb6c98dc4
unknown <angelotebh@hotmail.com>
2020-05-12 17:42:40 +0100

    Adds actions tests

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 build:
 	docker build --target base -t todos_admin_base .
 
+test:
+	docker run -it --rm todos_admin_base npm test
+
+test-ci:
+	docker run -it --rm --env CI=true todos_admin_base npm test
+
 build-release:
 	docker build --target release -t todos_admin_release --build-arg BACKEND_URL=${REACT_APP_BACKEND_URL} .
 
---
+++ src/actions/requests.test.js
@@ -0,0 +1,33 @@
+import { requestPending, requestDone, requestFailed, requestErrorShowed } from './';
+
+test('should generate request pending action', () => {
+    const action = requestPending();
+
+    expect(action).toEqual({
+        type: 'REQUEST_STARTED'
+    })
+})
+
+test('should generate request done action', () => {
+    const action = requestDone();
+
+    expect(action).toEqual({
+        type: 'REQUEST_DONE'
+    })
+})
+
+test('should generate request failed action', () => {
+    const action = requestFailed();
+
+    expect(action).toEqual({
+        type: 'REQUEST_FAILED'
+    })
+})
+
+test('should generate request error showed action', () => {
+    const action = requestErrorShowed();
+
+    expect(action).toEqual({
+        type: 'REQUEST_ERROR_SHOWED'
+    })
+})
