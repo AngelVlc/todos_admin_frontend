686f2992640a11ba2bdf01aeea5ea78d8e191c58
unknown <angelotebh@hotmail.com>
2020-05-16 11:41:39 +0100

    Adds user page tests

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { AppContext } from '../../contexts/AppContext';
 import { doPost, doPut, doGet } from '../../helpers/api';
 import * as Yup from 'yup';
@@ -9,104 +9,92 @@ export const UserPage = (props) => {
     const { auth, requestsDispatch } = useContext(AppContext)
     let { userId } = useParams();
     let history = useHistory();
-    const [user, setUser] = useState(null);
+
+    const initialState = {
+        title: 'New user',
+        submintBtnText: 'CREATE',
+        submitUrl: 'users',
+        isNew: true,
+        name: '',
+        isAdmin: 'no'
+    }
+    const [pageState, setPageState] = useState(initialState);
 
     useEffect(() => {
-        const getUser = async () => {
-            const res = await doGet(`users/${userId}`, auth.info.token, requestsDispatch)
-            setUser(res);
+        const getExistingUser = async () => {
+            const user = await doGet(`users/${userId}`, auth.info.token, requestsDispatch)
+
+            setPageState({
+                title: `Edit user '${user.name}' (is admin: ${user.isAdmin})`,
+                submintBtnText: 'SAVE',
+                submitUrl: `users/${userId}`,
+                isNew: false,
+                name: user.name,
+                isAdmin: user.isAdmin ? 'yes' : 'no'
+            });
         }
         if (auth.info && userId) {
-            getUser();
+            getExistingUser();
         }
     }, [userId, auth.info, requestsDispatch]);
 
-    var title = 'New user'
-    var submintBtnText = 'CREATE'
-    var postUrl = 'users'
-    var userName, isAdmin = ''
-    if (user) {
-        userName = user.name
-        isAdmin = user.isAdmin;
-        title = `Edit user '${userName}' (is admin: ${isAdmin})`
-        submintBtnText = 'SAVE';
-        postUrl = `users/${userId}`;
-    }
-
-    const formik = useFormik({
-        initialValues: {
-            name: userName,
-            isAdmin: isAdmin,
-            newPassword: '',
-            confirmNewPassword: ''
-        },
-        validationSchema: Yup.object({
-            name: Yup.string()
-                .required('Required')
-        }),
-        onSubmit: async (values) => {
-            if (!user) {
-                await doPost(postUrl, values, auth.info.token, requestsDispatch)
-            } else {
-                await doPut(postUrl, values, auth.info.token, requestsDispatch)
-            }
-            history.goBack();
-        }
-    });
-
     return (
         <>
-            <p>{userName}</p>
-            <h4>{title}</h4>
-            <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input
-                    id="name"
-                    name="name"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.name}
-                    autoComplete="off"
-                />
-                {formik.touched.name && formik.errors.name ? (
-                    <div>{formik.errors.name}</div>
-                ) : null}
-                <label htmlFor="newPassword">Password</label>
-                <input
-                    type="password"
-                    id="newPassword"
-                    name="newPassword"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.newPassword}
-                    autoComplete="off"
-                />
-                {formik.touched.newPassword && formik.errors.newPassword ? (
-                    <div>{formik.errors.newPassword}</div>
-                ) : null}
-                <label htmlFor="confirmNewPassword">Confirm Password</label>
-                <input
-                    type="password"
-                    id="confirmNewPassword"
-                    name="confirmNewPassword"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.confirmNewPassword}
-                    autoComplete="off"
-                />
-                <label htmlFor="isAdmin">Is Admin</label>
-                <input
-                    type="checkbox"
-                    id="isAdmin"
-                    name="isAdmin"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    checked={formik.values.isAdmin}
-                />
-                <button type="submit">{submintBtnText}</button>
-                <button type="button" onClick={() => history.push(`/user/${userId}/delete`)}>DELETE</button>
-                <button type="button" onClick={() => history.goBack()}>CANCEL</button>
-            </form>
+            <h4>{pageState.title}</h4>
+            <Formik
+                enableReinitialize={true}
+                initialValues={{
+                    name: pageState.name,
+                    isAdmin: pageState.isAdmin,
+                    newPassword: '',
+                    confirmNewPassword: ''
+                }}
+                validationSchema={
+                    Yup.object({
+                        name: Yup.string()
+                            .required('Required')
+                    })
+                }
+                onSubmit={async ({name, isAdmin, newPassword, confirmNewPassword}) => {
+                    const body = {
+                        name,
+                        newPassword,
+                        confirmNewPassword,
+                        isAdmin: isAdmin === 'yes' ? true : false
+                    }
+                    if (pageState.isNew) {
+                        await doPost(pageState.submitUrl, body, auth.info.token, requestsDispatch)
+                    } else {
+                        await doPut(pageState.submitUrl, body, auth.info.token, requestsDispatch)
+                    }
+                    history.goBack();
+                }}>
+                <Form>
+                    <label htmlFor="name">Name</label>
+                    <Field name="name" type="text" data-testid="name" />
+                    <span data-testid="userNameErrors">
+                        <ErrorMessage name="name" />
+                    </span>
+
+                    <label htmlFor="newPassword">Password</label>
+                    <Field name="newPassword" type="password" data-testid="newPassword" />
+
+                    <label htmlFor="confirmNewPassword">Confirm Password</label>
+                    <Field name="confirmNewPassword" type="password" data-testid="confirmNewPassword" />
+
+                    <label htmlFor="isAdmin">Is Admin</label>
+                    <Field name="isAdmin" as="select" data-testid="isAdmin">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </Field>
+
+                    <button data-testid="submit" type="submit">{pageState.submintBtnText}</button>
+                    {!pageState.isNew &&
+                        <button data-testid="delete" type="button" onClick={() => history.push(`/user/${userId}/delete`)}>DELETE</button>
+                    }
+                    <button data-testid="cancel" type="button" onClick={() => history.goBack()}>CANCEL</button>
+                </Form>
+            </Formik>
         </>
     )
 }
---
+++ src/components/UserPage/UserPage.test.js
@@ -0,0 +1,160 @@
+import React from 'react'
+import { render, cleanup, fireEvent, wait } from '@testing-library/react'
+import { UserPage } from './UserPage'
+import { AppContext } from '../../contexts/AppContext'
+import * as api from '../../helpers/api';
+import { MemoryRouter, Route } from 'react-router-dom'
+import { act } from 'react-dom/test-utils';
+
+jest.mock('../../helpers/api');
+const mockHistoryGoBack = jest.fn();
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        goBack: mockHistoryGoBack,
+        push: mockHistoryPush
+    })
+}));
+
+const renderWithContextAndRouterForExistingUser = (component) => {
+    api.doGet.mockResolvedValue({
+        id: 2,
+        name: 'user',
+        isAdmin: false
+    });
+    const context = { auth: { info: {} } };
+    return {
+        ...render(
+            <AppContext.Provider value={context}>
+                <MemoryRouter initialEntries={[`/user/2/edit`]}>
+                    <Route path="/user/:userId/edit">
+                        {component}
+                    </Route>
+                </MemoryRouter>
+            </AppContext.Provider>)
+    }
+}
+
+const renderWithContextAndRouterForNewUser = (component) => {
+    const context = { auth: { info: {} } };
+    return {
+        ...render(
+            <AppContext.Provider value={context}>
+                <MemoryRouter initialEntries={[`/user/new`]}>
+                    <Route path="/user/new">
+                        {component}
+                    </Route>
+                </MemoryRouter>
+            </AppContext.Provider>)
+    }
+}
+
+afterEach(cleanup)
+
+it('should match the snapshot for an existing user', async () => {
+    let fragment;
+    await act(async () => {
+        const { asFragment } = renderWithContextAndRouterForExistingUser(<UserPage />);
+        fragment = asFragment;
+    });
+    expect(fragment(<UserPage />)).toMatchSnapshot();
+});
+
+it('should match the snapshot for a new user', async () => {
+    const { asFragment } = renderWithContextAndRouterForNewUser(<UserPage />);
+    expect(asFragment(<UserPage />)).toMatchSnapshot();
+});
+
+it('should allow delete an existing user', async () => {
+    let container
+    await act(async () => {
+        container = renderWithContextAndRouterForExistingUser(<UserPage />);
+    });
+
+    await wait(() => {
+        fireEvent.click(container.getByTestId('delete'));
+    })
+
+    expect(mockHistoryPush.mock.calls.length).toBe(1);
+    expect(mockHistoryPush.mock.calls[0][0]).toBe('/user/2/delete');
+    mockHistoryPush.mockClear();
+});
+
+it('should allow cancel', async () => {
+    const { getByTestId } = renderWithContextAndRouterForNewUser(<UserPage />);
+
+    await wait(() => {
+        fireEvent.click(getByTestId('cancel'));
+    })
+
+    expect(mockHistoryGoBack.mock.calls.length).toBe(1);
+    mockHistoryGoBack.mockClear();
+});
+
+it('should require user name', async () => {
+    const { getByTestId } = renderWithContextAndRouterForNewUser(<UserPage />);
+
+    await wait(() => {
+        fireEvent.click(getByTestId('submit'));
+    })
+
+    expect(getByTestId('userNameErrors')).toHaveTextContent('Required');
+})
+
+it('should update an existing user', async () => {
+    let container
+    await act(async () => {
+        container = renderWithContextAndRouterForExistingUser(<UserPage />);
+    });
+
+    await changeInputValue(container.getByTestId, 'name', 'updated user');
+    await changeInputValue(container.getByTestId, 'isAdmin', true);
+    await changeInputValue(container.getByTestId, 'newPassword', 'pass');
+    await changeInputValue(container.getByTestId, 'confirmNewPassword', 'pass');
+
+    api.doPut.mockResolvedValue({});
+
+    await wait(() => {
+        fireEvent.click(container.getByTestId('submit'));
+    })
+
+    expect(api.doPut.mock.calls.length).toBe(1);
+    expect(api.doPut.mock.calls[0][0]).toBe('users/2');
+    expect(api.doPut.mock.calls[0][1]).toStrictEqual({ name: 'updated user', isAdmin: true, newPassword: 'pass', confirmNewPassword: 'pass' });
+
+    expect(mockHistoryGoBack.mock.calls.length).toBe(1);
+    mockHistoryGoBack.mockClear();
+});
+
+it('should create a new user', async () => {
+    const { getByTestId } = renderWithContextAndRouterForNewUser(<UserPage />);
+
+    await changeInputValue(getByTestId, 'name', 'new user');
+    await changeInputValue(getByTestId, 'newPassword', 'pass');
+    await changeInputValue(getByTestId, 'confirmNewPassword', 'pass');
+
+    api.doPost.mockResolvedValue({});
+
+    await wait(() => {
+        fireEvent.click(getByTestId('submit'));
+    })
+
+    expect(api.doPost.mock.calls.length).toBe(1);
+    expect(api.doPost.mock.calls[0][0]).toBe('users');
+    expect(api.doPost.mock.calls[0][1]).toStrictEqual({ name: 'new user', isAdmin: false, newPassword: 'pass', confirmNewPassword: 'pass' });
+
+    expect(mockHistoryGoBack.mock.calls.length).toBe(1);
+    mockHistoryGoBack.mockClear();
+});
+
+const changeInputValue = async (getByTestId, name, value) => {
+    await wait(() => {
+        fireEvent.change(getByTestId(name), {
+            target: {
+                value: value
+            }
+        })
+    })
+}
