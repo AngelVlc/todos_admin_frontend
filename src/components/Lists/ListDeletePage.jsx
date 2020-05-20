fd6ec8fad23e89a11d3a81839198d173c1d03a12
unknown <angelotebh@hotmail.com>
2020-05-20 18:29:27 +0100

    Adds list delete page

--- src/components/Lists/ListDeletePage.jsx
+++ src/components/Lists/ListDeletePage.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useContext, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { AppContext } from '../../contexts/AppContext';
+import { doGet, doDelete } from '../../helpers/api';
 
 export const ListDeletePage = () => {
+    const { auth, requestsDispatch } = useContext(AppContext)
+    const [list, setList] = useState(null);
+    let { listId } = useParams();
     let history = useHistory();
 
+    useEffect(() => {
+        const getList = async () => {
+            const res = await doGet(`lists/${listId}`, auth.info.token, requestsDispatch)
+            const info = {
+                ...res,
+                title: `Delete list ${res.name}`
+            }
+            setList(info);
+        }
+        if (auth.info) {
+            getList();
+        }
+    }, [listId, auth.info, requestsDispatch]);
+
+    const deleteList = async () => {
+        await doDelete(`lists/${listId}`, auth.info.token, requestsDispatch)
+        history.goBack();
+    }
+
     return (
-        <div className="container">
-            <h3 className="title">LIST DELETE PAGE</h3>
-            <button className="button" onClick={() => history.push('/')}>Home</button>
-        </div>
-    )
+        <>
+            {list &&
+                <div className="container">
+                    <h3 className="title">{list.title}</h3>
+                    <div className="buttons">
+                        <button className="button is-danger" data-testid="yes" onClick={() => deleteList()}>YES</button>
+                        <button className="button" data-testid="no" onClick={() => history.goBack()}>NO</button>
+                    </div>
+                </div>
+            }
+        </>
+    );
 }
---
+++ src/components/Lists/ListDeletePage.test.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { render, cleanup, fireEvent, wait } from '@testing-library/react'
+import { ListDeletePage } from './ListDeletePage'
+import { AppContext } from '../../contexts/AppContext'
+import * as api from '../../helpers/api';
+import { MemoryRouter, Route } from 'react-router-dom'
+import { act } from 'react-dom/test-utils';
+
+jest.mock('../../helpers/api');
+const mockHistoryGoBack = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        goBack: mockHistoryGoBack
+    })
+}));
+
+const renderWithContextAndRouter = (component) => {
+    api.doGet.mockResolvedValue({
+        id: 2,
+        name: 'ListName'
+    });
+    const context = { auth: { info: {} } };
+    return {
+        ...render(
+            <AppContext.Provider value={context}>
+                <MemoryRouter initialEntries={[`/lists/2/delete`]}>
+                    <Route path="/lists/:listId/delete">
+                        {component}
+                    </Route>
+                </MemoryRouter>
+            </AppContext.Provider>)
+    }
+}
+
+afterEach(cleanup)
+
+it('should match the snapshot', async () => {
+    let fragment;
+    await act(async () => {
+        const { asFragment } = renderWithContextAndRouter(<ListDeletePage />);
+        fragment = asFragment;
+    });
+    expect(fragment(<ListDeletePage />)).toMatchSnapshot();
+});
+
+it('should cancel the deletion', async () => {
+    let container
+    await act(async () => {
+        container = renderWithContextAndRouter(<ListDeletePage />);
+    });
+
+    await wait(() => {
+        fireEvent.click(container.getByTestId('no'));
+    })
+
+    expect(mockHistoryGoBack.mock.calls.length).toBe(1);
+    mockHistoryGoBack.mockClear();
+});
+
+it('should delete the List', async () => {
+    let container
+    await act(async () => {
+        container = renderWithContextAndRouter(<ListDeletePage />);
+    });
+
+    api.doDelete.mockResolvedValue({});
+
+    await wait(() => {
+        fireEvent.click(container.getByTestId('yes'));
+    })
+
+    expect(api.doDelete.mock.calls.length).toBe(1);
+    expect(api.doDelete.mock.calls[0][0]).toBe('lists/2');
+    expect(mockHistoryGoBack.mock.calls.length).toBe(1);
+    mockHistoryGoBack.mockClear();
+});
