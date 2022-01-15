30522c82e1e2a5cbbe22192fb133a357cba9e387
AngelVlc <angel.ballesteros@domestika.org>
2022-01-15 20:01:33 +0100

    Updates EditListItemPage

--- src/lists/infrastructure/components/Lists/EditListItemPage.jsx
+++ src/lists/infrastructure/components/Lists/EditListItemPage.jsx
@@ -1,38 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { ListItemForm } from './ListItemForm';
-import axios from 'axios';
+import React, { useEffect, useState, useContext, useCallback } from "react";
+import { Link, useParams } from "react-router-dom";
+import { ListItemForm } from "./ListItemForm";
+import { AppContext } from "../../../../shared/infrastructure/contexts";
+import { GetListItemByIdUseCase } from "../../../application/listItems";
 
 export const EditListItemPage = () => {
-    let { listId, itemId } = useParams();
+  let { listId, itemId } = useParams();
+  const { useCaseFactory } = useContext(AppContext);
+  const [pageState, setPageState] = useState({ title: "" });
 
-    const [pageState, setPageState] = useState({title: ''});
+  const getExistingItem = useCallback(async () => {
+    const getListItemByIdUseCase = useCaseFactory.get(GetListItemByIdUseCase);
+    const data = await getListItemByIdUseCase.execute(listId, itemId);
 
-    useEffect(() => {
-        const getExistingItem = async () => {
-            const res = await axios.get(`lists/${listId}/items/${itemId}`)
-            const item = res.data
+    setPageState(data);
+  }, [listId, itemId, useCaseFactory]);
 
-            setPageState({
-                title: item.title,
-                description: item.description
-            });
-        }
-        getExistingItem()
-    }, [listId, itemId]);
+  useEffect(() => {
+    getExistingItem();
+  }, [listId, itemId, getExistingItem]);
 
-    return (
-        <div className="container">
-            <h3 className="title">{`Edit item '${pageState.title}'`}</h3>
-            <nav className="breadcrumb" aria-label="breadcrumbs">
-                <ul>
-                    <li><Link to={`/`}>Home</Link></li>
-                    <li><Link to={`/lists`}>Lists</Link></li>
-                    <li><Link to={`/lists/${listId}/edit`}>List</Link></li>
-                    <li className="is-active"><Link aria-current="page" to={`/lists/${listId}/items/${itemId}/edit`}>{pageState.title}</Link></li>
-                </ul>
-            </nav>
-            <ListItemForm listId={listId} itemId={itemId} title={pageState.title} description={pageState.description} isNew={false} submintBtnText='SAVE' submitUrl={`lists/${listId}/items/${itemId}`}/>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <h3 className="title">{`Edit item '${pageState.title}'`}</h3>
+      <nav className="breadcrumb" aria-label="breadcrumbs">
+        <ul>
+          <li>
+            <Link to={`/`}>Home</Link>
+          </li>
+          <li>
+            <Link to={`/lists`}>Lists</Link>
+          </li>
+          <li>
+            <Link to={`/lists/${listId}/edit`}>List</Link>
+          </li>
+          <li className="is-active">
+            <Link
+              aria-current="page"
+              to={`/lists/${listId}/items/${itemId}/edit`}
+            >
+              {pageState.title}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <ListItemForm
+        listId={listId}
+        itemId={itemId}
+        title={pageState.title}
+        description={pageState.description}
+        isNew={false}
+        submintBtnText="SAVE"
+        submitUrl={`lists/${listId}/items/${itemId}`}
+      />
+    </div>
+  );
+};
--- src/lists/infrastructure/components/Lists/EditListItemPage.test.js
+++ src/lists/infrastructure/components/Lists/EditListItemPage.test.js
@@ -1,51 +1,60 @@
-import { render, cleanup } from '@testing-library/react';
-import { EditListItemPage } from './EditListItemPage';
-import { AppContext } from  '../../../../shared/infrastructure/contexts';
-import axios from 'axios';
-import { MemoryRouter, Route } from 'react-router-dom';
-import { act } from 'react-dom/test-utils';
+import { render, cleanup } from "@testing-library/react";
+import { EditListItemPage } from "./EditListItemPage";
+import { AppContext } from "../../../../shared/infrastructure/contexts";
+import { MemoryRouter, Route } from "react-router-dom";
+import { act } from "react-dom/test-utils";
+import { ListItem } from "../../../domain";
 
-jest.mock('axios');
+jest.mock("axios");
 const mockHistoryPush = jest.fn();
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useHistory: () => ({
-        push: mockHistoryPush
-    })
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
 }));
 
 const renderWithContextAndRouterForExistingItem = () => {
-    axios.get.mockResolvedValue(
-        {
-            data:
-            {
-                id: 2,
-                title: 'item title',
-                description: 'item description'
-            }
-        }
-    );
-    const context = { auth: { info: {} } };
-    return {
-        ...render(
-            <AppContext.Provider value={context}>
-                <MemoryRouter initialEntries={[`/lists/2/items/5/edit`]}>
-                    <Route path="/lists/:listId/items/:itemId/edit">
-                        <EditListItemPage/>
-                    </Route>
-                </MemoryRouter>
-            </AppContext.Provider>)
-    }
-}
+  const mockedGetListItemByIdUseCase = {
+    execute: jest.fn(),
+  };
 
-afterEach(cleanup)
+  const useCaseFactory = {
+    get: () => {
+      return mockedGetListItemByIdUseCase;
+    },
+  };
 
-it('should match the snapshot for an existing item', async () => {
-    let fragment;
-    await act(async () => {
-        const { asFragment } = renderWithContextAndRouterForExistingItem();
-        fragment = asFragment;
-    });
-    expect(fragment()).toMatchSnapshot();
+  mockedGetListItemByIdUseCase.execute.mockResolvedValue(
+    new ListItem({
+      id: 2,
+      title: "item title",
+      description: "item description",
+    })
+  );
+
+  const context = { auth: { info: {} }, useCaseFactory };
+  return {
+    ...render(
+      <AppContext.Provider value={context}>
+        <MemoryRouter initialEntries={[`/lists/2/items/5/edit`]}>
+          <Route path="/lists/:listId/items/:itemId/edit">
+            <EditListItemPage />
+          </Route>
+        </MemoryRouter>
+      </AppContext.Provider>
+    ),
+  };
+};
+
+afterEach(cleanup);
+
+it("should match the snapshot for an existing item", async () => {
+  let fragment;
+  await act(async () => {
+    const { asFragment } = renderWithContextAndRouterForExistingItem();
+    fragment = asFragment;
+  });
+  expect(fragment()).toMatchSnapshot();
 });
